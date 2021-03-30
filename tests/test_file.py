3f53fb83c6f635c06bf93286c957b43d8caef8ab
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-03-30 11:24:00 +0200

    Additions TestFile class on test_file module

---
+++ tests/test_file.py
@@ -0,0 +1,32 @@
+import unittest
+from tablib import Dataset
+from tempfile import gettempdir
+from unittest.mock import MagicMock, mock_open, patch
+
+
+class TestFile(unittest.TestCase):
+
+    def test_file(self):
+        # Simulate reports.io.File object
+        file = MagicMock()
+        file.raw_data = ['first line\n', 'second line\n', 'third line']
+        data = Dataset()
+        for line in file.raw_data:
+            data.append([line])
+        # Read file data
+        file.read = MagicMock(return_value=data)
+        lines = file.read()
+        self.assertIsInstance(lines, Dataset)
+        # Write file data
+        tmp_folder = gettempdir()
+        read_data = ''.join(file.raw_data)
+        with open(f'/{tmp_folder}/test_file.txt', 'w') as wf:
+            wf.write(read_data)
+        with patch('__main__.open', mock_open(read_data=read_data)):
+            with open(f'/{tmp_folder}/test_file.txt') as rf:
+                result = rf.read()
+        self.assertEqual(read_data, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
