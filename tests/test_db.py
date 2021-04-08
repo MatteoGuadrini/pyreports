f1854e93870113e835f8e6b6b9923443aca6fafa
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-04-08 12:04:06 +0200

    Additions test_mysql_connection method on TestDB class

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,16 +1,33 @@
 import unittest
 import reports
 from tablib import Dataset
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 
 class TestDB(unittest.TestCase):
 
     def test_connection(self):
-        # Simulate reports.io.Connection object
+        # reports.io.Connection object
         conn = reports.io.Connection()
         self.assertIsInstance(conn, reports.io.Connection)
 
+    def test_mysql_connection(self):
+        # Simulate reports.io.MySQLConnection object
+        conn = MagicMock()
+        with patch(target='mysql.connector.connect') as mock:
+            # Test connect
+            conn.connection = mock.return_value
+            conn.cursor = conn.connection.cursor.return_value
+            conn.connection.database = 'mydb'
+            conn.connection.username = 'username'
+            conn.connection.password = 'password'
+            conn.connection.port = 3306
+            self.assertEqual(conn.connection.database, 'mydb')
+            self.assertEqual(conn.connection.username, 'username')
+            self.assertEqual(conn.connection.password, 'password')
+            self.assertEqual(conn.connection.port, 3306)
+            # Test close
+            conn.cursor.close()
 
 
 if __name__ == '__main__':
