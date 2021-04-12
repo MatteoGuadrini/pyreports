1d4e06d8753290b7744a56015ba68ac3f7c25d35
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-04-12 16:50:54 +0200

    Additions test_get_data method on TestExecutor class

--- tests/test_core.py
+++ tests/test_core.py
@@ -10,6 +10,10 @@ class TestExecutor(unittest.TestCase):
     def test_executor_instance(self):
         self.assertIsInstance(self.data, reports.Executor)
 
+    def test_get_data(self):
+        self.assertIsInstance(self.data.get_data(), Dataset)
+        self.assertEqual(str(self.data.get_data()), 'Matteo|Guadrini|35')
+
 
 if __name__ == '__main__':
     unittest.main()
