f5e7dda23cb8bb586a4225f24dd152e08d7545ad
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-04-19 11:19:08 +0200

    Add ReportBook class on reports package

--- reports/__init__.py
+++ reports/__init__.py
@@ -23,6 +23,6 @@
 """Build complex reports from/to various formats."""
 
 from .io import manager
-from .core import Executor, Report
+from .core import Executor, Report, ReportBook
 
 __version__ = '0.0.8'
