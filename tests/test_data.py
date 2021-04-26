1fdcf678a4cef778f5e3d2f2e1ab7b943cd42403
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-04-26 11:15:14 +0200

    Add test_data unit test module

---
+++ tests/test_data.py
@@ -0,0 +1,15 @@
+import unittest
+import reports
+from tablib import Dataset
+
+
+class TestDataTools(unittest.TestCase):
+    data = Dataset(*[('Matteo', 'Guadrini', 35), ('Arthur', 'Dent', 42), ('Ford', 'Prefect', 42)])
+    data.headers = ['name', 'surname', 'age']
+
+    def test_average(self):
+        self.assertEqual(int(reports.average(self.data, 'age')), int(39.666666666666664))
+
+
+if __name__ == '__main__':
+    unittest.main()
