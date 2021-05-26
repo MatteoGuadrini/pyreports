4c766fd704332f8301ce68a2a20fe5ea44ecebcc
Matteo Guadrini <matteo.guadrini@hotmail.it>
2021-05-26 12:37:19 +0200

    First release

--- __info__.py
+++ __info__.py
@@ -22,7 +22,7 @@
 
 """Information variable used by modules on this package."""
 
-__version__ = '0.0.9'
+__version__ = '1.0.0'
 __author__ = 'Matteo Guadrini'
 __email__ = 'matteo.guadrini@hotmail.it'
 __homepage__ = 'https://github.com/MatteoGuadrini/pyreports'
