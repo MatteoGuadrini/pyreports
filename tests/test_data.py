ccf0d0a5c2d43b83270a8b092f68a8ffc6fb22db
Matteo Guadrini <matteo.guadrini@hotmail.it>
2024-03-06 09:06:51 +0100

    chore: add test_data_adapters function

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,14 +7,16 @@ from tablib import Dataset
 
 
 class TestDataTools(unittest.TestCase):
-    data = Dataset(*[('Matteo', 'Guadrini', 35), ('Arthur', 'Dent', 42), ('Ford', 'Prefect', 42)])
-    data.headers = ['name', 'surname', 'age']
+    data = Dataset(
+        *[("Matteo", "Guadrini", 35), ("Arthur", "Dent", 42), ("Ford", "Prefect", 42)]
+    )
+    data.headers = ["name", "surname", "age"]
 
     def test_average(self):
-        self.assertEqual(int(pyreports.average(self.data, 'age')), 39)
+        self.assertEqual(int(pyreports.average(self.data, "age")), 39)
 
     def test_most_common(self):
-        self.assertEqual(pyreports.most_common(self.data, 'age'), 42)
+        self.assertEqual(pyreports.most_common(self.data, "age"), 42)
 
     def test_percentage(self):
         self.assertEqual(int(pyreports.percentage(self.data, 42)), 66)
@@ -28,36 +30,63 @@ class TestDataTools(unittest.TestCase):
         names = self.data.get_col(0)
         surnames = self.data.get_col(1)
         ages = self.data.get_col(2)
-        self.assertEqual(pyreports.aggregate(names, surnames, ages)[0], ('Matteo', 'Guadrini', 35))
-        ages = ['Name', 'Surname']
-        self.assertRaises(tablib.InvalidDimensions, pyreports.aggregate, names,
-                          surnames, ages)
+        self.assertEqual(
+            pyreports.aggregate(names, surnames, ages)[0], ("Matteo", "Guadrini", 35)
+        )
+        ages = ["Name", "Surname"]
+        self.assertRaises(
+            tablib.InvalidDimensions, pyreports.aggregate, names, surnames, ages
+        )
         self.assertRaises(pyreports.ReportDataError, pyreports.aggregate, names)
 
     def test_aggregate_fill_empty(self):
         names = self.data.get_col(0)
         surnames = self.data.get_col(1)
-        ages = ['Name', 'Surname']
-        self.assertEqual(pyreports.aggregate(names, surnames, ages,
-                                             fill_empty=True)[2],
-                         ('Ford', 'Prefect', None))
+        ages = ["Name", "Surname"]
+        self.assertEqual(
+            pyreports.aggregate(names, surnames, ages, fill_empty=True)[2],
+            ("Ford", "Prefect", None),
+        )
 
     def test_chunks(self):
-        data = Dataset(*[('Matteo', 'Guadrini', 35), ('Arthur', 'Dent', 42), ('Ford', 'Prefect', 42)])
-        data.extend([('Matteo', 'Guadrini', 35), ('Arthur', 'Dent', 42), ('Ford', 'Prefect', 42)])
-        data.headers = ['name', 'surname', 'age']
-        self.assertEqual(list(pyreports.chunks(data, 4))[0][0], ('Matteo', 'Guadrini', 35))
+        data = Dataset(
+            *[
+                ("Matteo", "Guadrini", 35),
+                ("Arthur", "Dent", 42),
+                ("Ford", "Prefect", 42),
+            ]
+        )
+        data.extend(
+            [
+                ("Matteo", "Guadrini", 35),
+                ("Arthur", "Dent", 42),
+                ("Ford", "Prefect", 42),
+            ]
+        )
+        data.headers = ["name", "surname", "age"]
+        self.assertEqual(
+            list(pyreports.chunks(data, 4))[0][0], ("Matteo", "Guadrini", 35)
+        )
 
     def test_merge(self):
-        self.assertEqual(pyreports.merge(self.data, self.data)[3],
-                         ('Matteo', 'Guadrini', 35))
+        self.assertEqual(
+            pyreports.merge(self.data, self.data)[3], ("Matteo", "Guadrini", 35)
+        )
 
     def test_deduplication(self):
-        data = Dataset(*[('Matteo', 'Guadrini', 35),
-                         ('Arthur', 'Dent', 42),
-                         ('Matteo', 'Guadrini', 35)])
+        data = Dataset(
+            *[
+                ("Matteo", "Guadrini", 35),
+                ("Arthur", "Dent", 42),
+                ("Matteo", "Guadrini", 35),
+            ]
+        )
         self.assertEqual(len(pyreports.deduplicate(data)), 2)
 
+    def test_data_adapters(self):
+        data = pyreports.DataAdapters(Dataset(*[("Matteo", "Guadrini", 35)]))
+        self.assertIsInstance(data, pyreports.DataAdapters)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
